15c3cf7b60177292ccacad8692beacc46d754206
MohammadBseleh <mohammad.bseleh22@gmail.com>
2022-12-03 23:53:55 +0200

    this is task0 commit, http with hello world

---
+++ src/main/java/com/WebSer/institute/HelloWorldSpringWeb.java
@@ -0,0 +1,15 @@
+package com.WebSer.institute;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping ;
+import org.springframework.web.bind.annotation.RestController ;
+@RestController
+@RequestMapping (value = "/hello")
+
+public class HelloWorldSpringWeb {
+    @GetMapping
+   public String Hello()
+    {
+        return "Hello from Mohammad";
+    }
+
+}
