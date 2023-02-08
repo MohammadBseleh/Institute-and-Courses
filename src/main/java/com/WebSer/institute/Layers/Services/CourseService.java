9aa7c62f1299145fff7a58695d8de7bc53ff5909
MohammadBseleh <mohammad.bseleh22@gmail.com>
2023-02-08 04:02:20 +0200

    commit for task1

--- src/main/java/com/WebSer/institute/Layers/Services/CourseService.java
+++ src/main/java/com/WebSer/institute/Layers/Services/CourseService.java
@@ -1,4 +1,64 @@
 package com.WebSer.institute.Layers.Services;
 
+import com.WebSer.institute.Layers.DTO.CourseDTO;
+import com.WebSer.institute.Layers.Exeptions.DataNotFoundException;
+import com.WebSer.institute.Layers.Exeptions.InvalidInputException;
+import com.WebSer.institute.Layers.Repository.CourseRepository;
+import com.WebSer.institute.Layers.domains.Course;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CourseService {
+    private CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+    public CourseDTO readCourse(Long id)
+    {
+        isExist(id);
+        Course course = courseRepository.getCourse(id);
+        return new CourseDTO(course.getName(),course.getHours(),course.getLecturer(),course.getStartDate(),course.getEndDate());
+
+    }
+    public CourseDTO createCourse(CourseDTO courseDTO)
+    {
+        validDate(courseDTO);
+        Course course = courseRepository.createCourse(new Course(courseDTO.getName(),courseDTO.getHours(),courseDTO.getLecturer(),courseDTO.getStartDate(),courseDTO.getEndDate()));
+        return new CourseDTO(course.getName(),course.getHours(),course.getLecturer(),course.getStartDate(),course.getEndDate());
+    }
+    public CourseDTO updateCourse(Long id , CourseDTO courseDTO)
+    {
+        isExist(id);
+        validDate(courseDTO);
+        Course course = courseRepository.updateCourse(id , new Course(courseDTO.getName(),courseDTO.getHours(),courseDTO.getLecturer(),courseDTO.getStartDate(),courseDTO.getEndDate()));
+        return new CourseDTO(course.getName(),course.getHours(),course.getLecturer(),course.getStartDate(),course.getEndDate());
+
+    }
+    public void deleteCourse(Long id)
+    {
+        isExist(id);
+        courseRepository.deleteCourse(id);
+    }
+
+    private void isExist(Long id) {
+        if (courseRepository.getCourse(id) != null)
+        {
+            System.out.println("Course with id " + id + " is called");
+        }
+        else {
+
+            throw new DataNotFoundException("Course does not exist");
+
+        }
+    }
+
+    private void validDate(CourseDTO courseDTO) {
+        //Check if the date is valid
+        if (courseDTO.getStartDate().isAfter(courseDTO.getEndDate()))
+        {
+            throw new InvalidInputException("End Date Should be after The start date");
+        }
+    }
+
 }
--- src/main/java/com/WebSer/institute/Layers/Services/TeacherService.java
+++ src/main/java/com/WebSer/institute/Layers/Services/TeacherService.java
@@ -1,4 +1,52 @@
 package com.WebSer.institute.Layers.Services;
 
+import com.WebSer.institute.Layers.DTO.TeacherDTO;
+import com.WebSer.institute.Layers.Exeptions.DataNotFoundException;
+import com.WebSer.institute.Layers.Repository.TeacherRepository;
+import com.WebSer.institute.Layers.domains.Teacher;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TeacherService {
+    private TeacherRepository teacherRepository;
+
+    public TeacherService(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
+    public TeacherDTO readTeacher(Long id)
+    {
+        isExist(id);
+        Teacher teacher = teacherRepository.getTeacher(id);
+        return new TeacherDTO(teacher.getName(),teacher.getWorkingHours(),teacher.isFullTime(),teacher.getCourse());
+    }
+    public TeacherDTO createTeacher(TeacherDTO teacherDTO)
+    {
+
+        Teacher teacher = teacherRepository.createTeacher(new Teacher(teacherDTO.getName(),teacherDTO.getWorkingHours(),teacherDTO.isFullTime(),teacherDTO.getCourse()));
+        return new TeacherDTO(teacher.getName(),teacher.getWorkingHours(),teacher.isFullTime(),teacher.getCourse());
+    }
+    public void deleteTeacher(Long id)
+    {
+        isExist(id);
+        teacherRepository.deleteTeachers(id);
+    }
+    public TeacherDTO updateTeacher(Long id , TeacherDTO teacherDTO)
+    {
+        isExist(id);
+        Teacher teacher = teacherRepository.updateTeacher(id ,new Teacher(teacherDTO.getName(),teacherDTO.getWorkingHours(),teacherDTO.isFullTime(),teacherDTO.getCourse()));
+        return new TeacherDTO(teacher.getName(),teacher.getWorkingHours(),teacher.isFullTime(),teacher.getCourse());
+
+    }
+
+    private void isExist(Long id) {
+        if (teacherRepository.getTeacher(id) != null)
+        {
+            System.out.println("Teacher with id " + id + " is called");
+        }
+        else {
+
+            throw new DataNotFoundException("Teacher does not exist");
+
+        }
+    }
 }
